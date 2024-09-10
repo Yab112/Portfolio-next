7b718194bc14ae066082c95c9c2b6248ca0f2125
yabibal eshetie <106923105+Yab112@users.noreply.github.com>
2024-09-10 04:17:32 -0700

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,15 +2,11 @@ import Grid from "@/components/Grid";
 import Hero from "@/components/hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Image from "next/image";
-import { FaContao, FaHome } from "react-icons/fa";
-import { FaRProject } from "react-icons/fa6";
 import { ToastContainer } from 'react-toastify';
 import Clients from "@/components/Clients";
 import 'react-toastify/dist/ReactToastify.css';
 import Experiance from "@/components/Experiance";
 import Footer from "@/components/Footer";
-import { SignupFormDemo } from "@/components/SignupFormDemo";
 import { navItems } from "@/data";
 import RevelCard from "@/components/Footer2";
 
